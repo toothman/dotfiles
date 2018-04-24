718a927f69c68c34981c543ef8a86c251891894b
Sebastian Zahn <sebastian@zahnpost.de>
2018-04-24 09:58:38 +0200

    Add display configs

---
+++ screen/keller.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output VIRTUAL1 --off --output eDP1 --primary --mode 2560x1440 --pos 184x1620 --rotate normal --output DP1 --off --output HDMI2 --off --output HDMI1 --scale 1.5x1.5 --mode 1920x1080 --pos 0x0 --rotate normal --output DP2 --off
---
+++ screen/office.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output DP2-2 --primary --auto --scale 1x1 --pos 0x0 --rotate normal --output eDP1 --mode 2560x1440 --pos 3840x360 --rotate normal
